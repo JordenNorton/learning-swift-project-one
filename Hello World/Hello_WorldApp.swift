8819e1c5a1a750dfe706a1ec160fac8552f2ce74
jordennorton <jordennorton@hotmail.co.uk>
2024-06-09 02:04:32 +0100

    Initial Commit

---
+++ Hello World/Hello_WorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_WorldApp.swift
+//  Hello World
+//
+//  Created by Jorden Norton on 09/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct Hello_WorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
